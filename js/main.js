35819fcdc4415091f07fdc426ac652392f18904c
Nimit <nimitagg95@gmail.com>
2018-07-29 20:02:13 +0530

    Responsive main page
    Signed-off-by: Nimit <nimitagg95@gmail.com>

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibmFuZHViaGFpNzg5IiwiYSI6ImNqazVjdHQ0ejBpbXgzdnA2NmdyZXBvOXIifQ.bTzSXgZd1ucBfJmZRR44CQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -175,10 +175,13 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
+  const moreDiv = document.createElement('div');
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+
+  moreDiv.appendChild(more)
+  li.append(moreDiv)
 
   return li
 }
